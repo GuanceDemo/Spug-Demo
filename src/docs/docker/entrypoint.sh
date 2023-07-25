72bd6f4badca2f0af509d5f1e2a350ecbb500367
yangwenwei <494870806@qq.com>
2023-07-25 17:28:43 +0800

    修改dockerfile脚本

--- src/docs/docker/entrypoint.sh
+++ src/docs/docker/entrypoint.sh
@@ -3,14 +3,7 @@
 set -e
 
 
-#开启防火墙
-systemctl start firewalld
-#放行443端口
-firewall-cmd --zone=public --add-port=443/tcp --permanent
-#重新加载防火墙
-firewall-cmd --reload
-#查看是否放行成功
-firewall-cmd --zone=public --query-port=443/tcp
+
 
 
 if [ -e /root/.bashrc ]; then
@@ -18,18 +11,18 @@ if [ -e /root/.bashrc ]; then
 fi
 
 if [ ! -d /data/spug/spug_api ]; then
-    git clone  https://github.com/GuanceDemo/Spug-Demo.git /data/spug
+    #git clone  https://github.com/GuanceDemo/Spug-Demo.git /data/spug
     #curl -o web.tar.gz https://cdn.spug.cc/spug/web_${SPUG_DOCKER_VERSION}.tar.gz
     #tar xf web.tar.gz -C /data/spug/spug_web/
     #rm -f web.tar.gz
-    cp -rf /data/spug/src /data/spug
+
     SECRET_KEY=$(< /dev/urandom tr -dc '!@#%^.a-zA-Z0-9' | head -c50)
     cat > /data/spug/spug_api/spug/overrides.py << EOF
 import os
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 SECRET_KEY = '${SECRET_KEY}'
 
 DATABASES = {
