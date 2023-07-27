f48b350923d7e2d73b9f2a8731eb91ecdd05aec2
yangwenwei <494870806@qq.com>
2023-07-27 14:48:44 +0800

    加配置conf文件

--- src/spug_api/manage.py
+++ src/spug_api/manage.py
@@ -7,6 +7,10 @@ import os
 import sys
 from ddtrace import tracer
 import logging
+from configparser import ConfigParser
+
+cf = ConfigParser()
+cf.read("spug.conf")
 
 os.environ["DD_SERVICE"] = "Python-spug"    # 设置服务名
 os.environ["DD_ENV"] = "Testing"          # 设置环境名
@@ -16,15 +20,11 @@ os.environ["DD_REMOTE_CONFIGURATION_ENABLED"] = "true"  # 开启log注入
 os.environ["DD_AGENT_HOST"] = "47.106.191.26"  #datakit host
 os.environ["DD_AGENT_PORT"] = "9529"  #datakit port
 
-print(os.environ["DD_SERVICE"],)
-print(os.environ["DD_AGENT_HOST"],)
-print(os.environ["DD_AGENT_PORT"],)
-
-
 FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
           '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
           '- %(message)s')
-logging.basicConfig(level=logging.info, format=FORMAT)
+
+logging.basicConfig(level=logging.INFO,format=FORMAT)
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spug.settings')
