f79f4cbb2c25d0a37a50251c8e8d46a5849faeb0
yangwenwei <494870806@qq.com>
2023-07-27 17:41:52 +0800

    配置文件统一到spug.conf

--- src/spug_api/manage.py
+++ src/spug_api/manage.py
@@ -7,18 +7,26 @@ import os
 import sys
 from ddtrace import tracer
 import logging
+import os
 from configparser import ConfigParser
 
 cf = ConfigParser()
 cf.read("spug.conf")
+# ddtrace环境变量
+os.environ["DD_SERVICE"] = cf["ddtrace"]["dd_service"]   # 设置服务名
+os.environ["DD_ENV"] =  cf["ddtrace"]["dd_env"]           # 设置环境名
+os.environ["DD_VERSION"] = cf["ddtrace"]["dd_env"]          # 设置版本号
+os.environ["DD_LOGS_INJECTION"] = cf["ddtrace"]["dd_logs_injection"]  # 开启log注入
+os.environ["DD_REMOTE_CONFIGURATION_ENABLED"] = cf["ddtrace"]["dd_remote_configuration_enabled"]  # 开启log注入
+os.environ["DD_AGENT_HOST"] = cf["ddtrace"]["dd_agent_host"]   #datakit host
+os.environ["DD_AGENT_PORT"] = cf["ddtrace"]["dd_agent_port"]  #datakit port
 
-os.environ["DD_SERVICE"] = "Python-spug"    # 设置服务名
-os.environ["DD_ENV"] = "Testing"          # 设置环境名
-os.environ["DD_VERSION"] = "V1.1"         # 设置版本号
-os.environ["DD_LOGS_INJECTION"] = "true"  # 开启log注入
-os.environ["DD_REMOTE_CONFIGURATION_ENABLED"] = "true"  # 开启log注入
-os.environ["DD_AGENT_HOST"] = "47.106.191.26"  #datakit host
-os.environ["DD_AGENT_PORT"] = "9529"  #datakit port
+# mysql环境变量
+os.environ["MYSQL_DATABASE"] = cf["mysql"]["db_name"] 
+os.environ["MYSQL_USER"] = cf["mysql"]["db_user"] 
+os.environ["MYSQL_PASSWORD"] = cf["mysql"]["db_password"] 
+os.environ["MYSQL_HOST"] = cf["mysql"]["db_host"] 
+os.environ["MYSQL_PORT"] = cf["mysql"]["db_port"]
 
 FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
           '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
