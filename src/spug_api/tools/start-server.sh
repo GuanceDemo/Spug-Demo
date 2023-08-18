db3e496c8487a67ccc0fe256f1452946de7176d1
yangwenwei <494870806@qq.com>
2023-08-18 10:12:23 +0800

    test-workflow

--- src/spug_api/tools/start-server.sh
+++ src/spug_api/tools/start-server.sh
@@ -3,8 +3,16 @@
 # Copyright: (c) <spug.dev@gmail.com>
 # Released under the AGPL-3.0 License.
 # start worker service
-cd /data/spug/spug_api/
 
+echo "export DD_SERVICE=GC_MOBILE_SDK_DEMO" >> ~/.bashrc
+echo "export DD_SERVICE_MAPPING=postgres:postgresql,defaultdb:postgresql" >> ~/.bashrc
+echo "export DD_TAGS=project:mobie_sdk_demo,env:test,version:1.0" >> ~/.bashrc
+#echo "export DD_AGENT_HOST=https://pe-dk.guance.space" >> ~/.bashrc
+echo "export DD_AGENT_HOST=47.106.191.26" >> ~/.bashrc
+echo "export DD_AGENT_PORT=9529" >> ~/.bashrc
+source ~/.bashrc
+
+cd /data/spug/spug_api/
 python3 manage.py updatedb
 python3 manage.py user add -u admin -p admin -s -n admin
 
